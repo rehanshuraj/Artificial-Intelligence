0b3608c2eac4c847e46794024487d7e65b4a15be
callrajveer <rehanshugupta1234@gmail.com>
2025-10-16 15:13:34 +0530

    added

---
+++ Python/fileHandling.py
@@ -0,0 +1,8 @@
+# to open file in read mode
+file = open('file_name', 'mode')
+# to open file in write mode use 'w' thus it will overwrite the existing file first 
+# to open file in append mode use 'a'   
+# to open file in read and write mode use 'r+'
+# to open file in binary mode use 'b' at the end of mode like 'rb' or 'wb'
+# to open file in text mode use 't' at the end of mode like 'rt' or 'wt'
+# default mode is 'rt'
